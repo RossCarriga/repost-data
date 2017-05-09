ac3e2e47c18406b3f25e382d3a994d60ab5adb75
Thomas F <taforres@asu.edu>
2017-05-09 15:18:43 -0700

    Retry until valid reddit response

--- posts.py
+++ posts.py
@@ -2,8 +2,17 @@ import json
 import pprint
 import requests
 
-r = requests.get('http://www.reddit.com/r/cscareerquestions/top.json')
-response_json = r.json()
+def sample_valid_reddit_response():
+
+	r = requests.get('http://www.reddit.com/r/cscareerquestions/top.json')
+	response_json = r.json()
+
+	if 'data' not in response_json:
+		print("Trying again")
+		response_json = sample_valid_reddit_response()
+	return response_json
+
+response_json = sample_valid_reddit_response()
 
 with open('sample_response.json', 'w+') as f:
 	json.dump(response_json, f, indent=5)
