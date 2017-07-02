3769fa5571e8c5714dabfd1752ca5624abd68c76
Ross Carrigan <ross.carriga@gmail.com>
2017-07-01 17:29:47 -0700

    implemented redis to store json files.

--- posts.py
+++ posts.py
@@ -1,8 +1,12 @@
 import json
 import pprint
 import requests
+import pickle
+import redis
 
 SAMPLE_REDDIT_URL = 'http://www.reddit.com/r/cscareerquestions/top.json'
+REDIS_DB = redis.StrictRedis(host='localhost', port=6379, db=0)
+
 
 def sample_valid_reddit_response(url):
     r = requests.get(url)
@@ -12,28 +16,23 @@ def sample_valid_reddit_response(url):
         print("Trying again")
         response_json = sample_valid_reddit_response(url)
 
-    if 'children' in response_json['data']: 
-        del response_json['data']['children']
+    # if 'children' in response_json['data']:
+    #     del response_json['data']['children']
     else:
-        with open('sample_response.json', 'w+') as f:
-            json.dump(response_json, f, indent=5)
+        page_count = len(REDIS_DB.keys(pattern='*'))
+        response_json = pickle.dumps(response_json)
+        REDIS_DB.set(str(page_count), response_json)
+
     return response_json
 
-def get_next_reddit_response(sample):
-    response = {}    
-    after = sample['data']['after']
-    response_json = sample_valid_reddit_response(SAMPLE_REDDIT_URL + '?after=' + after)
-    return response_json
 
-def get_all_responses():
-    sample = sample_valid_reddit_response(SAMPLE_REDDIT_URL)
-    print(sample)
-    
-    while sample['data']['after'] != "null" and sample['data']['after'] != None:
-        sample = get_next_reddit_response(sample)
-        print(sample)
-    
-    print("end")
+# def get_next_reddit_response(sample):
+#     response = {}
+#     after = sample['data']['after']
+#     response_json = sample_valid_reddit_response(SAMPLE_REDDIT_URL + '?after=' + after)
+#     return response_json
 
-if '__main__' == __name__:
-    get_all_responses()
+sample_valid_reddit_response(SAMPLE_REDDIT_URL)
+with open('sample_response.json', 'w+') as f:
+    response = pickle.loads(REDIS_DB.get(0))
+    json.dump(response, f, indent=5)
